d27305fe7f81847b8287050824f1ed5147712bc4
Kim.Changbeom <Kim.Changbeom@N020557-230049.CANON-BS.CO.KR>
2024-05-16 15:47:56 +0900

    <변경 초안>

---
+++ kakaowork/src/main/java/com/example/demo/domain/MetadataVo.java
@@ -0,0 +1,64 @@
+package com.example.demo.domain;
+
+import java.util.List;
+
+public class MetadataVo {
+
+	private int _code;
+	private String _message;
+	private Object profile;
+	private Object synchronize_options;
+	//private String[] account_categories;
+	
+	
+
+	public MetadataVo(List ed, List sy) {
+		this._code = 200;
+		this._message = "ok";
+		this.profile = ed;
+		this.synchronize_options = sy;
+	}
+
+
+	public int get_code() {
+		return _code = 200;
+	}
+
+	public void set_code(int _code) {
+		this._code = 200;
+	}
+
+	public String get_message() {
+		return _message = "ok";
+	}
+
+	public void setReason(String _message) {
+		this._message = "ok";
+	}
+
+	public Object getprofile() {
+		return profile;
+	}
+
+	public void setprofile(Object profile) {
+		this.profile = profile;
+	}
+	
+	public Object getsynchronize_options() {
+		return synchronize_options;
+	}
+
+	public void setsynchronize_options(Object synchronize_options) {
+		this.synchronize_options = synchronize_options;
+	}
+	/*
+	public String[] getaccount_categories() {
+		return account_categories;
+	}
+
+	public void setaccount_categories(String[] account_categories) {
+		this.profile = profile;
+	}
+*/
+	
+}
