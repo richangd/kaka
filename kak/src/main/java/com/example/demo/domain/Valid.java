1d6a3b06af5bdfb6aab33b4349d3d4e5f2ad4095
Kim.Changbeom <Kim.Changbeom@N020557-230049.CANON-BS.CO.KR>
2024-05-28 11:42:17 +0900

    포맷 맞추기

--- kak/src/main/java/com/example/demo/domain/Valid.java
+++ kak/src/main/java/com/example/demo/domain/Valid.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 public class Valid {
 	
 	private int page_number;
-	private int page_size;
+	
 	
 		
 	public int getPage_number() {
@@ -16,13 +16,6 @@ public class Valid {
 	public void setPage_number(int page_number) {
 		this.page_number = page_number;
 	}
-	public int getPage_size() {
-		return page_size;
-	}
-	public void setPage_size(int page_size) {
-		this.page_size = page_size;
-	}
-	
 	
 	
 }
