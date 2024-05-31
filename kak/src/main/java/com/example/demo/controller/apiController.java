65861659bcbf02f9393d7c3435f8c24223131ed2
Kim.Changbeom <Kim.Changbeom@N020557-230049.CANON-BS.CO.KR>
2024-05-31 10:34:07 +0900

    검증테스트

--- kak/src/main/java/com/example/demo/controller/apiController.java
+++ kak/src/main/java/com/example/demo/controller/apiController.java
@@ -109,7 +109,7 @@ public class apiController {
         return re;
     }    
     
-	@GetMapping("/user/v0/getValidUsers")
+	@GetMapping("/user/v0/getValidUsers1")
 	public String getValidUsers() {			
     	
         return "{"
@@ -182,7 +182,7 @@ public class apiController {
 		
 	
 
-	@GetMapping("/user/v0/getValidUsers1")
+	@GetMapping("/user/v0/getValidUsers")
 	public ValidVo getValidUsers(@RequestBody Valid Val, @RequestHeader(value = "Kep-OrgLoginType", required = false) String OrgLoginType) {
         
 		ValidVo va = new ValidVo();
